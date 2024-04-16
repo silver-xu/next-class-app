af6ee662d71a438896061b586b2732c73e3c5d12
root <root@Ally>
2024-04-16 14:52:46 +1000

    Add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
-import { Button, ConfigProvider, Input } from "antd";
-import { SearchOutlined } from "@ant-design/icons";
+import {
+    Button,
+    ConfigProvider,
+    Dropdown,
+    Input,
+    MenuProps,
+    Space,
+} from "antd";
+import { SearchOutlined, DownOutlined } from "@ant-design/icons";
 
 import theme from "../theme/themeConfig";
 import styles from "./page.module.scss";
@@ -8,6 +15,24 @@ export const metadata = {
     title: "nextclass. || The interest of lifetime - Home",
     description: "nextclass. || The interest of lifetime - Home",
 };
+
+const items: MenuProps["items"] = [
+    {
+        label: <a href="https://www.antgroup.com">1st menu item</a>,
+        key: "0",
+    },
+    {
+        label: <a href="https://www.aliyun.com">2nd menu item</a>,
+        key: "1",
+    },
+    {
+        type: "divider",
+    },
+    {
+        label: "3rd menu item",
+        key: "3",
+    },
+];
 
 export default function Home() {
     return (
@@ -20,19 +45,15 @@ export default function Home() {
                     <h1>Find free trial classes.</h1>
                     <h2>The interest of lifetime.</h2>
                     <div className={styles.search}>
-                        <div className={styles.field}>
-                            <Input
-                                placeholder="Dancing, arts or skating"
-                                variant="borderless"
-                            />
-                        </div>
-                        <div className={styles.btn}>
-                            <Button
-                                type="primary"
-                                shape="circle"
-                                icon={<SearchOutlined />}
-                            />
-                        </div>
+                        <Input
+                            placeholder="Dancing, arts or skating"
+                            variant="borderless"
+                        />
+                        <Button
+                            type="primary"
+                            shape="circle"
+                            icon={<SearchOutlined />}
+                        />
                     </div>
                 </div>
                 <div className={styles.content}>
@@ -104,6 +125,37 @@ export default function Home() {
                             <p>Burwood, VIC</p>
                         </li>
                     </ul>
+                </div>
+                <div className={styles.footer}>
+                    <p>
+                        © 2024 Powered by the educo pty ltd. All rights
+                        reserved.
+                    </p>
+                    <ul>
+                        <li>
+                            <a href="https://www.google.com">
+                                Privacy Statement
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.google.com">
+                                Terms and Conditions
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.google.com">Contact Us</a>
+                        </li>
+                    </ul>
+                    <div className={styles.language}>
+                        <Dropdown menu={{ items }} trigger={["click"]}>
+                            <a>
+                                <Space>
+                                    Language: EN
+                                    <DownOutlined />
+                                </Space>
+                            </a>
+                        </Dropdown>
+                    </div>
                 </div>
             </div>
         </ConfigProvider>
