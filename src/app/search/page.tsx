83de52a845bb75046c24fe74638fb791565b6455
Silver.Xu <me@silverxu.au>
2024-05-19 13:28:30 +1000

    Fix Gallery

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -11,6 +11,25 @@ export const metadata = {
 };
 
 export default function Result() {
+    const searchResults = Array.from(Array(15).keys()).map(() => (
+        <li className={styles.searchResult}>
+            <div className={styles.infoWrapper}>
+                <h2>Rio Dance Studio</h2>
+                <span className={styles.ratingLabel}>5.0</span>
+                <Rate disabled value={5} className={styles.rating} />
+                <p className={styles.address}>
+                    327 Whitehorse Rd, Balwyn VIC 3103
+                </p>
+                <ul className={styles.gallery}>
+                    <li className={styles.gallery1}></li>
+                    <li className={styles.gallery2}></li>
+                    <li className={styles.gallery3}></li>
+                </ul>
+            </div>
+            <RightOutlined className={styles.chevron} />
+        </li>
+    ));
+
     return (
         <div className={styles.contentWrapper}>
             <Header theme="light" />
@@ -23,173 +42,7 @@ export default function Result() {
             </div>
 
             <div className={styles.searchResultWrapper}>
-                <ul>
-                    <li className={styles.searchResult}>
-                        <div className={styles.infoWrapper}>
-                            <h2>Rio Dance Studio</h2>
-                            <span className={styles.ratingLabel}>5.0</span>
-                            <Rate
-                                disabled
-                                value={5}
-                                className={styles.rating}
-                            />
-                            <p className={styles.address}>
-                                327 Whitehorse Rd, Balwyn VIC 3103
-                            </p>
-                        </div>
-                        <RightOutlined className={styles.chevron} />
-                    </li>
-                    <li className={styles.searchResult}>
-                        <div className={styles.infoWrapper}>
-                            <h2>Rio Dance Studio</h2>
-                            <span className={styles.ratingLabel}>5.0</span>
-                            <Rate
-                                disabled
-                                value={5}
-                                className={styles.rating}
-                            />
-                            <p className={styles.address}>
-                                327 Whitehorse Rd, Balwyn VIC 3103
-                            </p>
-                        </div>
-                        <RightOutlined className={styles.chevron} />
-                    </li>
-                    <li className={styles.searchResult}>
-                        <div className={styles.infoWrapper}>
-                            <h2>Rio Dance Studio</h2>
-                            <span className={styles.ratingLabel}>5.0</span>
-                            <Rate
-                                disabled
-                                value={5}
-                                className={styles.rating}
-                            />
-                            <p className={styles.address}>
-                                327 Whitehorse Rd, Balwyn VIC 3103
-                            </p>
-                        </div>
-                        <RightOutlined className={styles.chevron} />
-                    </li>
-                    <li className={styles.searchResult}>
-                        <div className={styles.infoWrapper}>
-                            <h2>Rio Dance Studio</h2>
-                            <span className={styles.ratingLabel}>5.0</span>
-                            <Rate
-                                disabled
-                                value={5}
-                                className={styles.rating}
-                            />
-                            <p className={styles.address}>
-                                327 Whitehorse Rd, Balwyn VIC 3103
-                            </p>
-                        </div>
-                        <RightOutlined className={styles.chevron} />
-                    </li>
-                    <li className={styles.searchResult}>
-                        <div className={styles.infoWrapper}>
-                            <h2>Rio Dance Studio</h2>
-                            <span className={styles.ratingLabel}>5.0</span>
-                            <Rate
-                                disabled
-                                value={5}
-                                className={styles.rating}
-                            />
-                            <p className={styles.address}>
-                                327 Whitehorse Rd, Balwyn VIC 3103
-                            </p>
-                        </div>
-                        <RightOutlined className={styles.chevron} />
-                    </li>
-                    <li className={styles.searchResult}>
-                        <div className={styles.infoWrapper}>
-                            <h2>Rio Dance Studio</h2>
-                            <span className={styles.ratingLabel}>5.0</span>
-                            <Rate
-                                disabled
-                                value={5}
-                                className={styles.rating}
-                            />
-                            <p className={styles.address}>
-                                327 Whitehorse Rd, Balwyn VIC 3103
-                            </p>
-                        </div>
-                        <RightOutlined className={styles.chevron} />
-                    </li>
-                    <li className={styles.searchResult}>
-                        <div className={styles.infoWrapper}>
-                            <h2>Rio Dance Studio</h2>
-                            <span className={styles.ratingLabel}>5.0</span>
-                            <Rate
-                                disabled
-                                value={5}
-                                className={styles.rating}
-                            />
-                            <p className={styles.address}>
-                                327 Whitehorse Rd, Balwyn VIC 3103
-                            </p>
-                        </div>
-                        <RightOutlined className={styles.chevron} />
-                    </li>
-                    <li className={styles.searchResult}>
-                        <div className={styles.infoWrapper}>
-                            <h2>Rio Dance Studio</h2>
-                            <span className={styles.ratingLabel}>5.0</span>
-                            <Rate
-                                disabled
-                                value={5}
-                                className={styles.rating}
-                            />
-                            <p className={styles.address}>
-                                327 Whitehorse Rd, Balwyn VIC 3103
-                            </p>
-                        </div>
-                        <RightOutlined className={styles.chevron} />
-                    </li>
-                    <li className={styles.searchResult}>
-                        <div className={styles.infoWrapper}>
-                            <h2>Rio Dance Studio</h2>
-                            <span className={styles.ratingLabel}>5.0</span>
-                            <Rate
-                                disabled
-                                value={5}
-                                className={styles.rating}
-                            />
-                            <p className={styles.address}>
-                                327 Whitehorse Rd, Balwyn VIC 3103
-                            </p>
-                        </div>
-                        <RightOutlined className={styles.chevron} />
-                    </li>
-                    <li className={styles.searchResult}>
-                        <div className={styles.infoWrapper}>
-                            <h2>Rio Dance Studio</h2>
-                            <span className={styles.ratingLabel}>5.0</span>
-                            <Rate
-                                disabled
-                                value={5}
-                                className={styles.rating}
-                            />
-                            <p className={styles.address}>
-                                327 Whitehorse Rd, Balwyn VIC 3103
-                            </p>
-                        </div>
-                        <RightOutlined className={styles.chevron} />
-                    </li>
-                    <li className={styles.searchResult}>
-                        <div className={styles.infoWrapper}>
-                            <h2>Rio Dance Studio</h2>
-                            <span className={styles.ratingLabel}>5.0</span>
-                            <Rate
-                                disabled
-                                value={5}
-                                className={styles.rating}
-                            />
-                            <p className={styles.address}>
-                                327 Whitehorse Rd, Balwyn VIC 3103
-                            </p>
-                        </div>
-                        <RightOutlined className={styles.chevron} />
-                    </li>
-                </ul>
+                <ul>{searchResults}</ul>
             </div>
         </div>
     );
