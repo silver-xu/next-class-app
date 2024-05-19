6fc5d3bca9cf6449065c8105eb57725416ad0b69
Silver.Xu <me@silverxu.au>
2024-05-19 22:10:16 +1000

    Add search page links

--- src/components/searchResult/searchResult.tsx
+++ src/components/searchResult/searchResult.tsx
@@ -3,18 +3,25 @@ import { Rate } from "antd";
 
 import styles from "./searchResult.module.scss";
 import { Gallery } from "../gallery";
+import Link from "next/link";
 
 export const SearchResult = (props: { startIndex: number }) => (
     <li className={styles.searchResult}>
-        <Gallery startIndex={props.startIndex} curveTop={true} />
-        <div className={styles.heading}>
-            <h2>Mock Arts Studio</h2>
-            <span className={styles.tag}>{150 * (props.startIndex + 1)}m</span>
-        </div>
-        <div className={styles.infoWrapper}>
-            <span className={styles.ratingLabel}>5.0</span>
-            <Rate disabled value={5} className={styles.rating} />
-            <p className={styles.address}>327 Whitehorse Rd, Balwyn VIC 3103</p>
-        </div>
+        <Link href="/listing">
+            <Gallery startIndex={props.startIndex} curveTop={true} />
+            <div className={styles.heading}>
+                <h2>Mock Arts Studio</h2>
+                <span className={styles.tag}>
+                    {150 * (props.startIndex + 1)}m
+                </span>
+            </div>
+            <div className={styles.infoWrapper}>
+                <span className={styles.ratingLabel}>5.0</span>
+                <Rate disabled value={5} className={styles.rating} />
+                <p className={styles.address}>
+                    327 Whitehorse Rd, Balwyn VIC 3103
+                </p>
+            </div>
+        </Link>
     </li>
 );
